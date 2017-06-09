ee903d031be53daa53e391c41fbeadc5e108d3e0
12ds95 <957517653@qq.com>
2017-06-09 20:01:48 +0800

    Update routes.js, index.js, add newsTable.jade

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,48 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.post('/signin', function (req, res, next) {
+    var username = req.body.user.name;
+    var password = req.body.user.password;
+    var code = parseInt(req.body.user.code);
+    if (req.session.checkcode === code) {
+
+    }
+});
+
+router.post('/news', function(req, res, next) {
+    var pageNum = req.body['pageNum'];
+    var itemPerPage = 20;
+    var result = getNews(pageNum, pageNum*itemPerPage);
+    res.json(result);
+});
+function getNews(from, to) {     // 取[from,to]的数据
+    var result;
+    var data = [
+        {
+            title: 'FFF团活动日',
+            author: '计算机科学与技术学院',
+            date: '5月20日',
+            content: '5.20呵呵呵呵'
+        },
+        {
+            title: '单身节',
+            author: '计算机科学与技术学院',
+            date: '11月11日',
+            content: '单身狗的节日'
+        }
+    ];
+    var head = ['title', 'author', 'date', 'content'];
+    var pageTotal = 4;
+    var title =  ['公告标题','发布单位','发布时间'];
+
+    result = {
+        'Title': title,
+        'Head': head,
+        'Content': data,
+        'pageTotal': pageTotal
+    };
+    return result;
+}
+
 module.exports = router;
