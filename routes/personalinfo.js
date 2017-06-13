724aac30282519b444bd8c16897d538fe33280e9
iqicheng <iqicheng@163.com>
2017-06-13 15:16:17 +0800

    个人信息修改页面更新
    增加post，并且现在个人信息修改栏中为现在库中的数据，尝试做个人信息和密码修改之间页面跳转，但目前失败，先上传。

--- routes/personalinfo.js
+++ routes/personalinfo.js
@@ -17,7 +17,29 @@ router.get('/', function(req, res, next) {
     res.render('personalinfo',{
         title : 'Personalinfo',
         studentID : '314010xxxx',
-        studentName : 'xxx'
+        studentName : 'xxx',
+        address : '浙江大学玉泉校区',
+        email : 'cailaoda@zju.edu.cn',
+        phone : '178xxxxxxxx'
+    });
+});
+
+router.post('/changeinfo', function(req, res, next) {
+    console.log(req.body);
+    var address = req.body.address;
+    var email = req.body.email;
+    var phone = req.body.phone;
+
+    var data={
+        status: 1
+    };
+    console.log(data);
+    res.json(data);
+});
+
+router.get('/changepw', function (req,res,next) {
+    res.render('changepw',{
+        title : "change password"
     });
 });
 
