8fcaf71d30f81dfe94a75a83a141d25ad1c70f5d
Mokoa <1102139175@qq.com>
2017-06-16 00:59:16 +0800

    add 报表

--- routes/inforeport.js
+++ routes/inforeport.js
@@ -7,7 +7,23 @@ var nodeExcel = require('excel-export');
 
 router.get('/', function(req, res, next) {
     var conf = {};
-    var data = [['3140102449','Qi',true,25.5,'计算机','iqicheng'],['3140102448','zhou',false, 30,'软件','ke']];
+    // var data = [['3140102448','zhou',false, 30,'软件','ke']];
+    var data=[
+    {
+        id:'3140102449',
+        name:'Qi',
+        sex:true,
+        credits:25.5,
+        department:'计算机',
+        userName:'iqicheng'
+    },{
+        id:'3140102448',
+        name:'zhou',
+        sex:false,
+        credits:30,
+        department:'软件',
+        userName:'ke'
+    }];
     conf.name = 'studentinfo';
     conf.cols = [{
         caption: 'id',
@@ -36,11 +52,14 @@ router.get('/', function(req, res, next) {
     conf.rows = data;
 
     var result = nodeExcel.execute(conf);
+    res.render('inforeport',{
+        data
+    })
     //console.log(result);
-    res.setHeader('Content-Type', 'application/vnd.openxmlformats');
-    res.setHeader("Content-Disposition", "attachment; filename= studentinfo.xlsx");
-    //res.setHeader('Content-Type', 'text/plain');
-    res.end(result, 'binary');
+    // res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+    // res.setHeader("Content-Disposition", "attachment; filename= studentinfo.xlsx");
+    // //res.setHeader('Content-Type', 'text/plain');
+    // res.end(result, 'binary');
 });
 
 module.exports = router;
