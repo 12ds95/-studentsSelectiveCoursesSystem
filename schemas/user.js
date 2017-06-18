f805d5b618948c1df3ee4b2e9f21836a6f99ad79
Anthony <1875898003@qq.com>
2017-06-18 23:01:32 +0800

    :

--- schemas/user.js
+++ schemas/user.js
@@ -20,10 +20,6 @@ var UserSchema = new mongoose.Schema({
 
 UserSchema.methods = {
     comparePassword: function(_password,cb){
-        console.log("_password");
-        console.log(_password);
-        console.log("this.password");
-        console.log(this.password);
         bcrypt.compare(_password,this.password,function (err,isMatch){
             if (err) {
                 return cb(err)
@@ -37,8 +33,6 @@ UserSchema.methods = {
 UserSchema.pre('save',function(next){
     var user = this;
     bcrypt.genSalt(SALT_WORK_FACTOR,function(err,salt){
-        console.log("genSalt");
-        console.log(salt);
         if (err){
             var innerr = new Error(err);
             next(innerr);
@@ -48,8 +42,6 @@ UserSchema.pre('save',function(next){
                 var innerr = new Error(err);
                 next(innerr);
             }
-            console.log("hash");
-            console.log(hash);
             user.password = hash;
             console.log('Saved password: '+hash);
             next()
@@ -59,23 +51,18 @@ UserSchema.pre('save',function(next){
 
 UserSchema.statics = {
     fetch: function(cb){
-        console.log("statics");
         return this
             .find({})
             .sort('name')
             .exec(cb)
     },
     findByName:function(name,cb){
-        console.log("findByName");
         return this
             .findOne({name:name})
             .exec(cb)
     },
     getUserType: function(uname,cb){
-        console.log("getUserType");
         this.findOne({name: uname},function(err,res){
-            console.log("findOne");
-            console.log(res);
             assert.equal(err,null);
             cb(null,res.user_type);
         });
